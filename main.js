e871a51531648b50e8a18346f5cbdf7d2e1ef6b0
stefanosoprana <stefano.soprana@icloud.com>
2018-12-11 18:25:17 +0100

    grid 6x6

---
+++ main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+  for (var i = 0; i < 36; i++) {
+
+    var square = $('.template .square').clone()
+
+    $('.square-container').append(square);
+    $(square).children().append()
+
+  }
+
+});
