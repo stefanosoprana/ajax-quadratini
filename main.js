4eaca603e00eba71406cecd30805733be264a7f3
stefanosoprana <stefano.soprana@icloud.com>
2018-12-11 19:46:48 +0100

    random number

--- main.js
+++ main.js
@@ -1,10 +1,26 @@
 $(document).ready(function(){
 
-  for (var i = 0; i < 36; i++) {
+  for (var i = 0; i < 37; i++) {
+
+    $.ajax({
+      url : 'https://www.boolean.careers/api/random/int',
+      method : 'GET',
+      success : function(data){
+
+        var numeroRandom = data.response;
+        console.log(numeroRandom);
+
+      },
+      error : function(){
+        console.log('errore');
+      },
+
+    });
+
+    $('.square-container').append(square);
 
     var square = $('.template .square').clone()
 
-    $('.square-container').append(square);
     $(square).children().append()
 
   }
